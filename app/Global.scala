6a9c134807c911c65ae0c72408e8ebc794bdb06e
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2016-03-02 23:46:55 +0200

    Load initial data on startup

--- app/Global.scala
+++ app/Global.scala
@@ -1,11 +1,9 @@
-import models._
-import play.api._
+import dao._
+import models.{Position, PlayType, Formation}
+import play.api.{Application, GlobalSettings}
+import slick.driver.H2Driver.api._
 
-/**
-  * Created on 2016-02-17.
-  *
-  * @author vili
-  */
+
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) = {
@@ -13,18 +11,32 @@ object Global extends GlobalSettings {
   }
 
   def loadTestData = {
-    ScoutDataDb.save(Formation("Split"))
-    ScoutDataDb.save(Formation("Slot"))
-    ScoutDataDb.save(Formation("Ace"))
-    ScoutDataDb.save(Formation("I"))
-    ScoutDataDb.save(Formation("Triple"))
-
-    val run = ScoutDataDb.save(PlayType("Run"))
-    val pass = ScoutDataDb.save(PlayType("Pass"))
-
-    ScoutDataDb.save(PlayInformation("Toss", run))
-    ScoutDataDb.save(PlayInformation("Lead", run))
-    ScoutDataDb.save(PlayInformation("Dive", run))
-    ScoutDataDb.save(PlayInformation("Hail Mary", pass))
+    val db = Database.forConfig("h2mem1")
+    db.run(
+      DBIO.seq(
+        Formations.objects.schema.create,
+        Formations.objects += Formation(None, "Split"),
+        Formations.objects += Formation(None, "Slot"),
+        Formations.objects += Formation(None, "Ace"),
+        Formations.objects += Formation(None, "I"),
+        Formations.objects += Formation(None, "Triple"),
+        PlayTypes.objects.schema.create,
+        PlayTypes.objects += PlayType(None, "Run"),
+        PlayTypes.objects += PlayType(None, "Pass"),
+        Positions.objects.schema.create,
+        Positions.objects += Position(None, "OL"),
+        Positions.objects += Position(None, "QB"),
+        Positions.objects += Position(None, "RB"),
+        Positions.objects += Position(None, "WR"),
+        Positions.objects += Position(None, "TE"),
+        Positions.objects += Position(None, "DL"),
+        Positions.objects += Position(None, "LB"),
+        Positions.objects += Position(None, "CB"),
+        Positions.objects += Position(None, "S"),
+        PlayInformations.objects.schema.create,
+        DownInformations.objects.schema.create,
+        Players.objects.schema.create
+      )
+    )
   }
 }
