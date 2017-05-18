7a373b829c8432ab92f356ba79e0c98339fb165b
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2017-05-18 22:40:20 +0300

    Adds more information to logged requests

--- app/filters/LoggingFilter.scala
+++ app/filters/LoggingFilter.scala
@@ -6,7 +6,8 @@ import akka.stream.Materializer
 import play.api.Logger
 import play.api.mvc._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Await}
+import scala.concurrent.duration._
 
 class LoggingFilter @Inject() (implicit val mat: Materializer, val ec: ExecutionContext) extends Filter {
   def apply(nextFilter: RequestHeader => Future[Result])
@@ -19,7 +20,16 @@ class LoggingFilter @Inject() (implicit val mat: Materializer, val ec: Execution
       val endTime = System.currentTimeMillis
       val requestTime = endTime - startTime
 
-      Logger.info(s"${requestHeader.method} ${requestHeader.uri} - ${requestTime}ms - RETURN: ${result.header.status}")
+      val loggingFuture = result.body.consumeData.map(_.utf8String).map { str =>
+
+        val dataStr =  str.stripPrefix("\n\n").startsWith("<") match {
+          case true => str.replace("\n", "")
+          case false => str
+        }
+
+        Logger.info(s"${requestHeader.method} ${requestHeader.uri} - ${requestTime}ms - RETURN: ${result.header.status} - DATA: ${dataStr}")
+      }
+      Await.result(loggingFuture, 5000 millis)
 
       result.withHeaders("Request-Time" -> requestTime.toString)
     }
