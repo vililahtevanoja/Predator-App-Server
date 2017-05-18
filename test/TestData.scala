da35f52e6381958fd5b2db201098f59fbb22961d
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2017-05-18 22:45:27 +0300

    Modifies test data

--- test/TestData.scala
+++ test/TestData.scala
@@ -1,7 +1,7 @@
 import javax.inject.Inject
 
 import dao._
-import models.{Position, Formation, PlayType}
+import models.{Position, Formation, PlayType, Team, Player}
 import play.api.db.slick.DatabaseConfigProvider
 import slick.driver.JdbcProfile
 
@@ -12,15 +12,19 @@ class TestData @Inject()(dbConfigProvider: DatabaseConfigProvider) {
     dbConfig.db.run(
       DBIO.seq(
         Formations.objects.schema.create,
+        PlayTypes.objects.schema.create,
+        Positions.objects.schema.create,
+        Teams.objects.schema.create,
+        PlayInformations.objects.schema.create,
+        DownInformations.objects.schema.create,
+        Players.objects.schema.create,
         Formations.objects += Formation(None, "Split"),
         Formations.objects += Formation(None, "Slot"),
         Formations.objects += Formation(None, "Ace"),
         Formations.objects += Formation(None, "I"),
         Formations.objects += Formation(None, "Triple"),
-        PlayTypes.objects.schema.create,
         PlayTypes.objects += PlayType(None, "Run"),
         PlayTypes.objects += PlayType(None, "Pass"),
-        Positions.objects.schema.create,
         Positions.objects += Position(None, "OL"),
         Positions.objects += Position(None, "QB"),
         Positions.objects += Position(None, "RB"),
@@ -30,9 +34,10 @@ class TestData @Inject()(dbConfigProvider: DatabaseConfigProvider) {
         Positions.objects += Position(None, "LB"),
         Positions.objects += Position(None, "CB"),
         Positions.objects += Position(None, "S"),
-        PlayInformations.objects.schema.create,
-        DownInformations.objects.schema.create,
-        Players.objects.schema.create
+        Teams.objects += Team(Some(1), "Aalto Predators"),
+        Teams.objects += Team(Some(2), "Lappeenranta Wildmen"),
+        Players.objects += Player(Some(1), 1, 66),
+        Players.objects += Player(Some(2), 1, 69)
       )
     )
   }
